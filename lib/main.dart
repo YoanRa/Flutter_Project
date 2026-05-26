6a047b2cb107a7a72545c8eb0c2c533be8fbce5b
YoanRa <yoaboret1@gmail.com>
2026-05-25 21:38:15 -0500

    Ejercicio Formulario

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/formulario.dart';
 import 'package:flutter_project/trabajo.dart';
 
 void main() {
-  runApp(const Trabajo());
+  runApp(const RegistroUsuario());
 }
